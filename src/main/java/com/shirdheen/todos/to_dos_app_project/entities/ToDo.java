356bd1bbe882e9bfbceedec6abeb2a1938602389
shirdheen <shirdheen2252@gmail.com>
2025-02-22 17:33:06 +1100

    Adds lombok library, table entities

---
+++ src/main/java/com/shirdheen/todos/to_dos_app_project/entities/ToDo.java
@@ -0,0 +1,40 @@
+package com.shirdheen.todos.to_dos_app_project.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "todos")
+public class ToDo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String taskName;
+
+    @Column(nullable = false)
+    private boolean isCompleted = false;
+
+    @Column(nullable = false)
+    private boolean isArchived = false;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+}
