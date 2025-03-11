fee4b5987c8e446579080c16bd71e2f7da6a5ac3
shirdheen <shirdheen2252@gmail.com>
2025-03-12 09:45:39 +1100

    Refactors modal

--- todo-app-frontend/src/subcomponents/Modal/Modal.tsx
+++ todo-app-frontend/src/subcomponents/Modal/Modal.tsx
@@ -1,24 +1,22 @@
 import { ReactNode } from "react";
 import styles from "./Modal.module.scss";
 import ReactDOM from "react-dom";
+import ModalActions from "../ModalActions/ModalActions";
 
 interface ModalProps {
   title: string;
   children: ReactNode;
   onClose: () => void;
+  actions: React.ReactNode;
 }
 
-const Modal = ({ title, children, onClose }: ModalProps) => {
+const Modal = ({ title, children, onClose, actions }: ModalProps) => {
   return ReactDOM.createPortal(
     <div className={styles.modalOverlay}>
       <div className={styles.modalContent}>
         <h3 className={styles.modalHeading}>{title}</h3>
         <div className={styles.modalBody}>{children}</div>
-        <div className={styles.modalActions}>
-          <button onClick={onClose} className={styles.cancelButton}>
-            Close
-          </button>
-        </div>
+        <ModalActions onClose={onClose}>{actions}</ModalActions>
       </div>
     </div>,
     document.body
