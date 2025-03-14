5982d1c8a3e27bcf50118666740ac9310780f986
shirdheen <shirdheen2252@gmail.com>
2025-03-14 14:58:19 +1100

    Update Vite config with base path for GitHub Pages

--- todo-app-frontend/vite.config.ts
+++ todo-app-frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/nology-todo-app/",
   plugins: [react()],
   server: {
     port: 5173,
